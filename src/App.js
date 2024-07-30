fc7999a14da1d5728ab5751530a68043ccf4fbae
beatrizpcampos <beatrizcampos300@gmail.com>
2024-07-30 14:04:14 -0300

    starting code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from "react-icons/fi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep aqui"
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>
+        </h2>
+      </main>
     </div>
   );
 }
